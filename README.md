004ad9467b9e4d49059927075d1b56dc585f510c
Snooman205 <76412950+Snooman205@users.noreply.github.com>
2022-07-17 21:56:11 +0000

    Added some of the base code

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Game engine made entirely in C#
 The project is open source so you can use the source code for your own project just please credit me
 You can also contribute to the project. Any contribution will help
+
+
+# Note from developer
+Hey, this is currently in the works to be optimized for the game titled Cave Survival
--- main.cs
+++ main.cs
@@ -6,12 +6,44 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Main
 {
-    class main {
-        public Scene1()
-{
-    graphics = new GraphicsDeviceManager(this);
-    Content.RootDirectory = "Content";
-}
+    public class Game1: Game{
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        protected override void Init()
+        {
+            //TODO: Generate seeded caves system
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+        
+        protected override void Update()
+        {
+            
+
+            base.Update();
+        }
+        
+        protected override void Draw()
+        {
+
+
+            base.Draw();
+        }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
 
     }
- }
+    
+
+    
+}
+
